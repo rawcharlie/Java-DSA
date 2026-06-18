22e22bedb60f749b28b667271f0283feabd02ff2
rawcharlie <aryanarihat@gmail.com>
2026-06-18 12:22:20 +0530

    Number utility

--- NumberUtility.java
+++ NumberUtility.java
@@ -18,8 +18,7 @@ public class NumberUtility {
         return true;
     }
     
-
-    public static int factorial(int n){
+    public static int factorial(int n){  
         int fact=1;
         for(int i=1; i<=n; i++){
             fact *= i;
